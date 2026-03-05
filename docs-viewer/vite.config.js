300162847152e24a094d6b7d6d1680bf89624a27
Lewis Ginn <Lewis.GINN@qed.qld.gov.au>
2026-03-05 21:24:09 +1100

    updated docs generator to produce flat HTML file on build

--- docs-viewer/vite.config.js
+++ docs-viewer/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
+  resolve: {
+    alias: {
+      'react': resolve('./node_modules/react'),
+      'react-dom': resolve('./node_modules/react-dom'),
+    }
+  }
 })
